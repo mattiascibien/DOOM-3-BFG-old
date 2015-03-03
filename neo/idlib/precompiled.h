98aa1b42038be30777e553173c4102c73ad55713
Mattias Cibien <mattias@mattiascibien.net>
2015-03-03 11:41:06 +0100

    Added Visual Leak Detector to all required projects

--- neo/idlib/precompiled.h
+++ neo/idlib/precompiled.h
@@ -160,4 +160,8 @@ const int MAX_EXPRESSION_REGISTERS = 4096;
 #undef max
 #include <algorithm>	// for min / max / swap
 
+#ifdef HAS_VLD
+#include <vld.h>
+#endif
+
 #endif /* !__PRECOMPILED_H__ */
